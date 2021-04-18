06478cd6a4063a862655efd5b1b3cc60fd0f5a39
Khumor Chang <mercchang@gmail.com>
2021-04-18 00:30:56 -0400

    initial commit

---
+++ src/app/PrimeNG.module.ts
@@ -0,0 +1,108 @@
+import { NgModule } from '@angular/core';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CardModule } from 'primeng/card';
+import { MenuModule } from 'primeng/menu';
+import { InputMaskModule } from 'primeng/inputmask';
+import { PanelModule } from 'primeng/panel';
+import { SpinnerModule } from 'primeng/spinner';
+import { EditorModule } from 'primeng/editor';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TabViewModule } from 'primeng/tabview';
+import { ListboxModule } from 'primeng/listbox';
+import { TooltipModule } from 'primeng/tooltip';
+import { CheckboxModule } from 'primeng/checkbox';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { AccordionModule } from 'primeng/accordion';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {FileUploadModule} from 'primeng/fileupload';
+import {StepsModule} from 'primeng/steps';
+import {CalendarModule} from 'primeng/calendar';
+import {GalleriaModule} from 'primeng/galleria';
+import {SliderModule} from 'primeng/slider';
+import { ChipModule } from 'primeng/chip';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [
+        InputTextareaModule,
+        InputTextModule,
+        InputMaskModule,
+        ButtonModule,
+        TableModule,
+        DialogModule,
+        ToastModule,
+        RadioButtonModule,
+        DropdownModule,
+        ProgressSpinnerModule,
+        CardModule,
+        MenuModule,
+        PanelModule,
+        SpinnerModule,
+        EditorModule,
+        FieldsetModule,
+        ConfirmDialogModule,
+        FieldsetModule,
+        TabViewModule,
+        ListboxModule,
+        TooltipModule,
+        CheckboxModule,
+        OverlayPanelModule,
+        AccordionModule,
+        MessageModule,
+        MessagesModule,
+        FileUploadModule,
+        StepsModule,
+        CalendarModule,
+        GalleriaModule,
+        SliderModule,
+        ChipModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        InputTextareaModule,
+        InputTextModule,
+        InputMaskModule,
+        ButtonModule,
+        TableModule,
+        DialogModule,
+        ToastModule,
+        RadioButtonModule,
+        DropdownModule,
+        ProgressSpinnerModule,
+        CardModule,
+        MenuModule,
+        PanelModule,
+        SpinnerModule,
+        EditorModule,
+        FieldsetModule,
+        ConfirmDialogModule,
+        FieldsetModule,
+        TabViewModule,
+        ListboxModule,
+        TooltipModule,
+        CheckboxModule,
+        OverlayPanelModule,
+        AccordionModule,
+        MessageModule,
+        MessagesModule,
+        FileUploadModule,
+        StepsModule,
+        CalendarModule,
+        GalleriaModule,
+        SliderModule,
+        ChipModule,
+        ReactiveFormsModule
+    ]
+})
+
+export class PrimeNGModule { }
